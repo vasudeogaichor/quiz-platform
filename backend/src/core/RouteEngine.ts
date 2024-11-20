055150fec456f55d8e20121b36cafa0561705c00
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2024-11-20 21:18:10 +0530

    backend: fix headers being sent twice

--- backend/src/core/RouteEngine.ts
+++ backend/src/core/RouteEngine.ts
@@ -75,7 +75,9 @@ export default class RouteEngine {
     return async (req: Request, res: Response, next: NextFunction) => {
       try {
         const result = await handler(req, res, next);
-        res.json(result);
+        // console.log("result - ", result);
+        // res.json(result);
+        return result;
       } catch (error) {
         console.log("error - ", error);
         next(error);
