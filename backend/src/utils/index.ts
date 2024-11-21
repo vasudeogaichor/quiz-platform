5ffdb286e5b360b89fb39187db39c9e61a4cabe9
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2024-11-21 08:22:41 +0530

    backend: optimize auth token generation

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -1,12 +1,25 @@
+import jwt from "jsonwebtoken";
 import { UserDTO } from "../types/controllers";
+import { IUser } from "../models/User";
 
 export const toUserDTO = (user: any): UserDTO => {
-    return {
-      id: user._id.toString(),
-      email: user.email,
-      fullName: user.fullName,
-      grade: user.grade,
-      createdAt: user.createdAt,
-      updatedAt: user.updatedAt,
-    };
+  return {
+    id: user._id.toString(),
+    email: user.email,
+    fullName: user.fullName,
+    grade: user.grade,
+    createdAt: user.createdAt,
+    updatedAt: user.updatedAt,
   };
+};
+
+export function generateAuthToken(
+  user: IUser,
+  expiresIn: string | number = "12h"
+): string {
+  return jwt.sign(
+    { id: user._id, grade: user.grade },
+    process.env.JWT_SECRET as string,
+    { expiresIn }
+  );
+}
