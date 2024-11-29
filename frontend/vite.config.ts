60d329d37a45ad247529f7829c26742a77ae84c5
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2024-11-29 12:04:58 +0530

    frontend: remove base path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/quiz-master/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
